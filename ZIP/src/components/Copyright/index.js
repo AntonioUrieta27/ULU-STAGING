a870dc1ee338184d9e7627e1f0f017968a97ecdc
Antonio Urieta <jsanchez@quaxar.com>
2022-09-09 11:02:23 -0500

    X

--- ZIP/src/components/Copyright/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.sass";
-
-const SalveCopyright = () => {
-  return <div id="copyright">Made with ❤ by Salve Agency</div>;
-};
-
-export default SalveCopyright;
--- ZIP/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const clientAxios = axios.create({
-    baseURL: 'https://ulu-back-storytelling.herokuapp.com/'
-	//https://ulu-back-storytelling.herokuapp.com
-    //http://localhost:4000
-});
-
-export default clientAxios;
